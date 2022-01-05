b0a0f97fdb7743d6332546dab7dae037396a9ee4
Enar <enar.joesaar@khk.ee>
2022-01-05 14:05:45 +0200

    Person klass ja tema objektid

---
+++ praks2/person.py
@@ -0,0 +1,14 @@
+class Person():
+    name = "Person name"
+    age = 0
+    def setPerson(self, name, age):
+        self.name = name
+        self.age = age
+
+    def birthday(self):
+        self.age = self.age + 1
+
+    def info(self):
+        print(self.name)
+        print(self.age)
+
---
+++ praks2/person_test.py
@@ -0,0 +1,11 @@
+from praks2.person import Person
+
+enar = Person()
+enar.setPerson("Enar", 16)
+enar.info()
+enar.age = enar.age + 1
+enar.info()
+
+margus = Person()
+margus.setPerson("Margus", 45)
+margus.info()
